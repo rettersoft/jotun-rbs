e5a530ea7b2381fa32ad73b2e255b269c581a9e9
Semih Ayhan <smhayhan@gmail.com>
2022-04-29 14:12:09 +0300

    wip

--- react-native-example/src/screens/home.tsx
+++ react-native-example/src/screens/home.tsx
@@ -2,14 +2,46 @@ import * as React from 'react'
 import { Button, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
+import useRio from '../hooks/useRio'
 import useAuth from '../hooks/useAuth'
 
 const HomeScreen = () => {
+    const rio = useRio()
     const auth = useAuth()
     const navigation = useNavigation()
 
     React.useEffect(() => {
-        console.log('methods: ', auth.userObject?.methods)
+        // console.log('methods: ', auth.userObject?.methods)
+
+        ;(async () => {
+            const a = await rio.getCloudObject({
+                classId: 'Training',
+                body: {
+                    userId: '5pvqivsot',
+                },
+            })
+
+            const b = await a.call<any>({
+                method: 'getTrainings',
+            })
+            const id = 'aE5_oGsE' // b.data.items[0].id
+            console.log(
+                'b: ',
+                b.data.items.map((i: any) => ({ id: i.id, status: i.status }))
+            )
+
+            const c = await a.call<any>({
+                method: 'startTraining',
+                body: { id },
+            })
+            console.log('c', c.data)
+
+            const d = await a.call<any>({
+                method: 'nextStep',
+                body: { id },
+            })
+            console.log('d', d.data)
+        })()
     }, [])
 
     return (
